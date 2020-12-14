b28e2cd498a731f317e1f0e852b17a17bc7642e1
alexpts <alexpts@yandex.ru>
2020-12-14 14:08:38 +0300

    Fix order of processors

--- test/unit/ServiceDiTest.php
+++ test/unit/ServiceDiTest.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+use DocDoc\Logger\Processor\CleanContextProcessor;
+use DocDoc\Logger\Processor\EsIndexProcessor;
 use DocDoc\Logger\SocketJsonHandler;
 use DocDoc\SymfonyDiLoader\LoaderContainer;
 use PHPUnit\Framework\TestCase;
@@ -24,6 +26,12 @@ class ServiceDiTest extends TestCase
         @unlink($this->cacheFile);
     }
 
+    protected function getContainer(): ContainerInterface
+    {
+        $loader = new LoaderContainer;
+        return $loader->getContainer([dirname(__DIR__, 2) . '/src/logger.yml'], $this->cacheFile);
+    }
+
     public function testCreateLoggerFromYaml(): void
     {
         $container = $this->getContainer();
@@ -41,9 +49,18 @@ class ServiceDiTest extends TestCase
         static::assertInstanceOf(SocketJsonHandler::class, $socketJsonHandler);
     }
 
-    protected function getContainer(): ContainerInterface
+    public function testProcessorOrderOnHandler(): void
     {
-        $loader = new LoaderContainer;
-        return $loader->getContainer([dirname(__DIR__, 2) . '/src/logger.yml'], $this->cacheFile);
+        $container = $this->getContainer();
+        $logger = $container->get(LoggerInterface::class);
+
+        $handler = $logger->getHandlers()[0];
+        $prop = new ReflectionProperty(SocketJsonHandler::class, 'processors');
+        $prop->setAccessible(true);
+        $processors = $prop->getValue($handler);
+
+        static::assertCount(2, $processors);
+        static::assertInstanceOf(EsIndexProcessor::class, $processors[0]);
+        static::assertInstanceOf(CleanContextProcessor::class, $processors[1]);
     }
 }
